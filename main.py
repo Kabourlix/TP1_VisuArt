2c16c09502b9dfdcbfc661b730bc98d1e1de2ed2
Kabourlix <hdamaia@outlook.fr>
2023-02-01 21:04:25 -0500

    min code to check if cv2 is installed

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import cv2
 import numpy as np
 
+test = np.zeros((100, 100, 3), np.uint8)
+test[0:50, 0:50] = (255, 0, 0)
+test[50:100, 50:100] = (0, 255, 0)
+
+cv2.imshow("test", test)
+cv2.waitKey(0)
