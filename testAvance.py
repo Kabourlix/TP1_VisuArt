8ec588db770fda74717f9f45a7ef3abe5426306f
Kabourlix <hdamaia@outlook.fr>
2023-02-15 11:16:01 -0500

    little changing on subtracting

--- testAvance.py
+++ testAvance.py
@@ -55,51 +55,51 @@ plt.imshow(foregroundMask, cmap='gray')
 plt.title('Masked difference between the two images')
 plt.show()
 
+foregroundMask = foregroundMask.astype(np.uint8)
 
 
+#
+# Dilate operation on foreground mask
+kernel = np.ones((5,5),np.uint8)
+dilated = cv2.dilate(foregroundMask, kernel, iterations=1)
 
-#
-# # Dilate operation on foreground mask
-# kernel = np.ones((5,5),np.uint8)
-# dilated = cv2.dilate(foregroundMask, kernel, iterations=1)
-#
-# # Opening operation on foreground mask
-# opening = cv2.morphologyEx(foregroundMask, cv2.MORPH_OPEN, kernel)
-# opening2 = cv2.morphologyEx(opening, cv2.MORPH_OPEN, kernel)
-#
-# # Plot results
-# plt.figure()
-# plt.subplot(221)
-# plt.imshow(foregroundMask, cmap='gray')
-# plt.title('Masked init')
-# plt.subplot(222)
-# plt.imshow(dilated, cmap='gray')
-# plt.title('Dilated mask')
-# plt.subplot(223)
-# plt.imshow(opening, cmap='gray')
-# plt.title('Opening mask')
-# plt.subplot(224)
-# plt.imshow(opening2, cmap='gray')
-# plt.title('Opening mask 2')
-# plt.show()
-#
-#
-# #get the contours of the masked image
-# contours, hierarchy = cv2.findContours(foregroundMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-#
-# #Get a bounding box for each contour
-# boundingBoxes = [cv2.boundingRect(c) for c in contours]
-# #Sort the bounding boxes by area
-# boundingBoxes = sorted(boundingBoxes, key=lambda x: x[2]*x[3], reverse=True)
-# #filter the bounding boxes by keeping min area
-# minArea = 100
-# boundingBoxes = [box for box in boundingBoxes if box[2]*box[3] > minArea]
-#
-# #Draw the bounding boxes on the original image
-# for box in boundingBoxes:
-#     cv2.rectangle(img, (box[0], box[1]), (box[0]+box[2], box[1]+box[3]), (0,255,0), 2)
-#
-# plt.figure()
-# plt.imshow(img)
-# plt.title('Contours on the original image')
-# plt.show()
+# Opening operation on foreground mask
+opening = cv2.morphologyEx(foregroundMask, cv2.MORPH_OPEN, kernel)
+opening2 = cv2.morphologyEx(opening, cv2.MORPH_OPEN, kernel)
+
+# Plot results
+plt.figure()
+plt.subplot(221)
+plt.imshow(foregroundMask, cmap='gray')
+plt.title('Masked init')
+plt.subplot(222)
+plt.imshow(dilated, cmap='gray')
+plt.title('Dilated mask')
+plt.subplot(223)
+plt.imshow(opening, cmap='gray')
+plt.title('Opening mask')
+plt.subplot(224)
+plt.imshow(opening2, cmap='gray')
+plt.title('Opening mask 2')
+plt.show()
+
+
+#get the contours of the masked image
+contours, hierarchy = cv2.findContours(foregroundMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+#Get a bounding box for each contour
+boundingBoxes = [cv2.boundingRect(c) for c in contours]
+#Sort the bounding boxes by area
+boundingBoxes = sorted(boundingBoxes, key=lambda x: x[2]*x[3], reverse=True)
+#filter the bounding boxes by keeping min area
+minArea = 100
+boundingBoxes = [box for box in boundingBoxes if box[2]*box[3] > minArea]
+
+#Draw the bounding boxes on the original image
+for box in boundingBoxes:
+    cv2.rectangle(img, (box[0], box[1]), (box[0]+box[2], box[1]+box[3]), (0,255,0), 2)
+
+plt.figure()
+plt.imshow(img)
+plt.title('Contours on the original image')
+plt.show()
